c142b5577bfb29ce9eb29a7d933f282b02ad9daf
kaku0o0 <43189700+kaku0o0@users.noreply.github.com>
2023-02-04 04:22:45 -0500

    Finish Statistic page, Config page

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -12,6 +12,10 @@ import os
 import glob
 
 import requests
+from plotly.offline import plot
+import plotly.express as px
+import plotly.graph_objs as go
+from flask import Markup
 
 def connect_to_database():
     return mysql.connector.connect(user=db_config['user'],
@@ -46,7 +50,7 @@ def upload_image():
     db_con =  get_db()
     cursor= db_con.cursor()
     #Check the key is not duplicate, if it is, replace the old file
-    cursor.execute("SELECT image_key FROM image_key_table1 WHERE image_key = %s GROUP BY image_key",(file_name,))
+    cursor.execute("SELECT image_key FROM image_key_table1 WHERE BINARY image_key = BINARY %s GROUP BY image_key",(file_name,))
     exist = cursor.fetchone()
     # add the key to the list of known keys in the database
     if exist is None:
@@ -64,7 +68,6 @@ def upload_image():
         os.makedirs("saved_images")
         print("Saved images directory is created!")
     file.save(file_path)
-    db_con.close()
 
     #invalidate memcache key
     memcache_invalidate_request = requests.post("http://localhost:5001/invalidate_key", data={'key': file_name })
@@ -92,13 +95,12 @@ def show_image():
     file_name = request.form.get("text")
     db_con =  get_db()
     cursor= db_con.cursor()
-    cursor.execute("SELECT image_key FROM image_key_table1 WHERE image_key = %s GROUP BY image_key",(file_name,))
+    cursor.execute("SELECT image_key FROM image_key_table1 WHERE BINARY image_key = BINARY %s GROUP BY image_key",(file_name,))
     exist = cursor.fetchone()
 
     # check if key exist in database
     if exist is None:
         return render_template('message.html', user_message = "The key you specified does not exist in the database", return_addr='/show_image')
-    db_con.close()
 
     # check if key exist in mem cache
     url="http://localhost:5001/get_image/"+file_name
@@ -137,9 +139,8 @@ def available_keys():
     db_con =  get_db()
     cursor= db_con.cursor()
     cursor.execute("SELECT * FROM image_key_table1")
-    placeholder_list = [row[0] for row in cursor.fetchall()]
-    db_con.close()
-    return render_template("show_keylist.html",list_title='All Available Keys', input_list=placeholder_list, return_addr='/')
+    database_key_list = [row[0] for row in cursor.fetchall()]
+    return render_template("show_keylist.html",list_title='All Available Keys', input_list=database_key_list, return_addr='/')
 
 @webapp.route('/available_keys',methods=['POST'])
 def key_deletion():
@@ -150,7 +151,6 @@ def key_deletion():
     cursor.execute("DELETE FROM image_key_table1")
     cursor.execute('SET SQL_SAFE_UPDATES = 1;')
     db_con.commit()
-    db_con.close()
     #Clear local system
     images = glob.glob('saved_images/*')
     for im in images:
@@ -158,7 +158,7 @@ def key_deletion():
 
     #Clear Memcache
     memcache_clear_request = requests.post("http://localhost:5001/clear_cache", data={})
-    print("Memcache clear: "+memcache_clear_request.text())
+    print("Memcache clear: "+memcache_clear_request.text)
 
     return available_keys()
 
@@ -171,25 +171,65 @@ def process_config():
     print(request.form)
     if 'display' in request.form:
         # TODO: contact mem cache to get the list of keys
-        placeholder_list = ['item1', 'item2', 'item3']
-        return render_template("show_list.html", list_title='All Keys In MemCache (PLACEHOLDER ONLY)', input_list=placeholder_list, return_addr='/config')
+        memcache_getkeys_request = requests.get("http://localhost:5001/get_lru_keys")
+        print("Memcache Get kes: ")
+        print(memcache_getkeys_request.json())
+        mecmcache_key_list = memcache_getkeys_request.json()
+        return render_template("show_list.html", list_title='All Keys In MemCache', input_list=mecmcache_key_list, return_addr='/config')
     elif 'clear' in request.form:
-        # TODO: send the clear message to mem cache
+        #send the clear message to mem cache
+        memcache_clear_request = requests.post("http://localhost:5001/clear_cache", data={})
+        print("Memcache clear: "+memcache_clear_request.text)
         return render_template("config.html", message = "Cache Cleared!")
     cache_size = request.form.get("cache_size")
     cache_size_float = float(cache_size)
     print(cache_size)
+
+    db_con =  get_db()
+    cursor= db_con.cursor()
+    cursor.execute('UPDATE cache_config SET capacity =  %s WHERE id = %s',(cache_size,1))
     # the option_selected has 2 possible values (both string): RR and LRU
     option_selected = request.form.get("policyRadios")
     print(option_selected)
+    cursor.execute('UPDATE cache_config SET replacement_policy = %s WHERE id = %s',(option_selected,1))
+    db_con.commit()
     # TODO: write the options to database and call refreshConfiguration() for memcache
+
+    memcache_refresh_request = requests.post("http://localhost:5001/refresh_configuration")
+
+    print("Memcache refresh: "+memcache_refresh_request.text)
+
     return render_template("config.html")
 
 @webapp.route('/statistics')
 def statistics():
     # TODO: Contact Database to read statistics
-    placeholder_list = ["Number of Items in Cache: 43", "Cache Size Used: 102.5MB", "Number of Request Served: 72", "Hit Rate: 50%", "Miss Rate: 50%"]
-    return render_template("show_list.html", list_title='MemCache Statistics (PLACEHOLDER ONLY)', input_list=placeholder_list, return_addr='/')
+    db_con =  get_db()
+    cursor= db_con.cursor()
+    cursor.execute('SELECT * FROM ( SELECT * FROM cache_stats ORDER BY id DESC LIMIT %s) AS sub ORDER BY id ASC',(121,))
+    cache_count=[]
+    cache_size=[]
+    miss_rate=[]
+    hit_rate=[]
+    requests_served=[]
+    for row in cursor.fetchall():
+        cache_count.append(row[2])
+        cache_size.append(row[3])
+        miss_rate.append(row[4])
+        hit_rate.append(row[5])
+        requests_served.append(row[6])
+
+  
+    x_axis = list(range(0, 605, 5)) 
+    cache_count_plot = plot({"data":go.Scatter(x=x_axis, y=cache_count), "layout": go.Layout(title = "Cache Count", xaxis_title = "Time(s)",yaxis_title = "Number of Cache")},output_type='div')
+    cache_size_plot = plot({"data":go.Scatter(x=x_axis, y=cache_size), "layout": go.Layout(title = "Cache Size", xaxis_title = "Time(s)",yaxis_title = "MB")},output_type='div')
+    miss_rate_plot = plot({"data":go.Scatter(x=x_axis, y=miss_rate), "layout": go.Layout(title = "Miss Rate", xaxis_title = "Time(s)",yaxis_title = "Rate")},output_type='div')
+    hit_rate_plot = plot({"data":go.Scatter(x=x_axis, y=hit_rate), "layout": go.Layout(title = "Hit Rate", xaxis_title = "Time(s)",yaxis_title = "Rate")},output_type='div')
+    requests_served_plot = plot({"data":go.Scatter(x=x_axis, y=requests_served), "layout": go.Layout(title = "Requests Served", xaxis_title = "Time(s)",yaxis_title = "Number of Request")},output_type='div')
+    
+
+    plot_list = [Markup(cache_count_plot),Markup(cache_size_plot),Markup(miss_rate_plot),Markup(hit_rate_plot),Markup(requests_served_plot)]
+    return render_template("show_list.html", list_title='MemCache Statistics', input_list=plot_list, return_addr='/')
 
 @webapp.route('/navbar')
 def global_navbar():
