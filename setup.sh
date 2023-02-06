3d457c2d637a60837f24d79a4d3f8e2ed290ca23
kaku0o0 <43189700+kaku0o0@users.noreply.github.com>
2023-02-06 01:17:23 -0500

    Update start.sh

---
+++ setup.sh
@@ -0,0 +1,9 @@
+echo "This script will set up a python virtual env in the dir that it is provoked, pleasd make sure your python3 command is available"
+sudo apt-get -y install libmysqlclient-dev
+echo "Installing Virtual Environment module"
+python3 -m pip install virtualenv
+echo "Setting up venv"
+python3 -m venv env
+source env/bin/activate
+echo "Installing required packages"
+python3 -m pip install -r requirements.txt
