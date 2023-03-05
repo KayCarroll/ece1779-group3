30cde5165c11b46fe7f5d6436667274113fe269a
KayCarroll <kaycarrollmcneil@gmail.com>
2023-03-04 21:11:29 -0500

    initial memcache updates for assignment 2 - needs testing

--- memcache/app/__init__.py
+++ memcache/app/__init__.py
@@ -1,5 +1,6 @@
 import atexit
 import json
+import os
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
@@ -8,6 +9,9 @@ from flask_sqlalchemy import SQLAlchemy
 from app.memcache import MemCache
 
 CONFIG_FILE = 'config.json'
+CACHE_ID = os.environ.get('CACHE_ID')
+CACHE_HOST = os.environ.get('CACHE_HOST')
+# CACHE_PORT = os.environ.get('CACHE_PORT')
 
 webapp = Flask(__name__)
 webapp.config.from_file(CONFIG_FILE, load=json.load)
@@ -15,11 +19,12 @@ webapp.config.from_file(CONFIG_FILE, load=json.load)
 db = SQLAlchemy()
 db.init_app(webapp)
 
-cache = MemCache()
+cache = MemCache(CACHE_ID)
 
-from app.main import set_initial_cache_config, store_memcache_statistics
+from app.main import set_initial_cache_config, set_cache_status, store_memcache_statistics
 
 set_initial_cache_config()
+set_cache_status()
 
 scheduler = BackgroundScheduler()
 scheduler.add_job(store_memcache_statistics, trigger='interval', seconds=5)
--- memcache/app/main.py
+++ memcache/app/main.py
@@ -1,14 +1,18 @@
+import boto3
 import logging
 
 from flask import json, request
-from app import webapp, db, cache
-from app.models import CacheConfig, CacheStats
+from app import webapp, db, cache, CACHE_HOST
+from app.models import CacheConfig, CacheStatus
 
+CLOUDWATCH_NAMESPACE = 'MemCache Metrics'
 LOG_FORMAT = '%(asctime)s - %(name)s - [%(levelname)s] - %(message)s'
+
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT, handlers=[logging.StreamHandler()])
 logging.getLogger('apscheduler').setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
+boto_client = boto3.client('cloudwatch')
 
 
 def set_initial_cache_config():
@@ -18,13 +22,17 @@ def set_initial_cache_config():
     return config
 
 
-def store_memcache_statistics():
+def set_cache_status():
     with webapp.app_context():
-        # TODO: Do some checks/tests to make sure that using the db object in this function while
-        # its is being run in a separate thread by the scheduler won't be an issue.
-        stats = CacheStats(**cache.get_statistics())
-        db.session.add(stats)
+        status = CacheStatus(id=cache.id, is_active=cache.is_active, cache_host=CACHE_HOST)
+        db.session.add(status)
         db.session.commit()
+
+
+def store_memcache_statistics():
+    # TODO: Consider checking if memcache is active and only storing metrics for active memcache.
+    metric_data = [{'MetricName': key, 'Value': val} for key, val in cache.get_statistics().items()]
+    boto_client.put_metric_data(Namespace=CLOUDWATCH_NAMESPACE, MetricData=metric_data)
 
 
 @webapp.route('/get_image/<key>', methods=['GET'])
@@ -114,4 +122,32 @@ def refresh_configuration():
     else:
         response = webapp.response_class(response=json.dumps('Config not found'), status=400,
                                          mimetype='application/json')
+    return response
+
+
+@webapp.route('/activate', methods=['POST'])
+def activate_cache():
+    """Activate the cache if not already active.
+    """
+    # TODO: Add logging and check if any additional logic needed to activate.
+    if not cache.is_active:
+        cache.activate()
+        set_cache_status()
+
+    response = webapp.response_class(response=json.dumps('OK'), status=200,
+                                     mimetype='application/json')
+    return response
+
+
+@webapp.route('/deactivate', methods=['POST'])
+def deactivate_cache():
+    """Deactivate the cache if currently active.
+    """
+    # TODO: Add logging and check if any additional logic needed to deactivate.
+    if cache.is_active:
+        cache.deactivate()
+        set_cache_status()
+
+    response = webapp.response_class(response=json.dumps('OK'), status=200,
+                                     mimetype='application/json')
     return response
