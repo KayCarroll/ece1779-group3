f91016d2f48e03e8494667697fb4d5a27a9aa543
Brandon <brandon.nie00@outlook.com>
2023-03-05 18:03:31 -0500

    Added cloud watch write and read in tests.py

--- manager_app/app/tests.py
+++ manager_app/app/tests.py
@@ -25,21 +25,37 @@ import boto3
 def tests():
     # response = s3_client.upload_file("./tmp/hello.txt", S3_bucket_name, "my_first_upload.txt")
     # print (response)
-    metric_data = [{'MetricName': "ID0", 'Value': 20}, {'MetricName': "ID1", 'Value': 15}]
+    my_dict = {'cache_id': 2, 'is_active': int(True), 'cache_count': 15,
+                'cache_size': 32, 'hit_rate': 0.8, 'miss_rate': 0.2,
+                'requests_served': 46}
+    instance_id = "2"
+    # metric_data = [
+    #     {
+    #         'MetricName': "hit_rate",
+    #         'Dimensions': [
+    #             {
+    #                 'Name': 'ID',
+    #                 'Value': '0'
+    #             },
+    #         ],
+    #         'Value': 0.6
+    #     },
+    #     {
+    #         'MetricName': "hit_rate",
+    #         'Dimensions': [
+    #             {
+    #                 'Name': 'ID',
+    #                 'Value': '1'
+    #             },
+    #         ],
+    #         'Value': 0.2
+    #     }
+    # ]
+    metric_data = [{'MetricName': key, 'Dimensions': [{'Name': 'ID', 'Value': instance_id}], 'Value': val} for key, val in my_dict.items()]
     response = cloudwatch_client.put_metric_data(Namespace='MemCache Metrics', MetricData=metric_data)
     print(response)
     response = cloudwatch_client.list_metrics(Namespace='MemCache Metrics')
     print(response)
-    metric_name = 'ID0'
-    memcache_metrics = cloudwatch_client.get_metric_statistics(
-            Period=1 * 60,
-            StartTime=datetime.utcnow() - timedelta(seconds=100 * 60),
-            EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
-            MetricName=metric_name,
-            Statistics=['Sum'],
-            Namespace='MemCache Metrics',  # Unit='Percent')
-    )
-    print(memcache_metrics)
     response = cloudwatch_client.get_metric_data(
         MetricDataQueries=[
             {
@@ -47,17 +63,23 @@ def tests():
                 'MetricStat': {
                     'Metric': {
                         'Namespace': 'MemCache Metrics',
-                        'MetricName': 'ID0',
+                        'MetricName': 'hit_rate',
+                        'Dimensions': [
+                            {
+                                'Name': 'ID',
+                                'Value': '2'
+                            },
+                        ]
                     },
-                    'Period': 60,
+                    'Period': 1*60,
                     'Stat': 'Average',
                 },
                 'ReturnData': True,
             },
         ],
-        StartTime=datetime.utcnow() - timedelta(seconds=100 * 60),
+        StartTime=datetime.utcnow() - timedelta(seconds=200 * 60),
         EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
-        ScanBy='TimestampDescending',
+        ScanBy='TimestampAscending',
     )
     print(response)
     return render_template("message.html", user_message = "Updated a metric", return_addr = '/')
