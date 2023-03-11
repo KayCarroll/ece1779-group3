13d0f559a9e7f914c2b2cfdb34ac911b3d6a9f18
KayCarroll <kaycarrollmcneil@gmail.com>
2023-03-11 11:29:56 -0500

    fixed the config attributes

--- autoscaler/app/autoscaler.py
+++ autoscaler/app/autoscaler.py
@@ -7,12 +7,12 @@ class ScalingMode(Enum):
 
 
 class AutoScaler:
-    def __init__(self, mode, max_miss_rate=0.8, min_miss_rate=0.2, expand_pool_factor=2, shrink_pool_factor=0.5):
+    def __init__(self, mode, max_miss_rate=0.8, min_miss_rate=0.2, expand_ratio=2, shrink_ratio=0.5):
         self.mode = mode
-        self.max_miss_rate = None
-        self.min_miss_rate = None
-        self.expand_pool_factor = None
-        self.shrink_pool_factor = None
+        self.max_miss_rate = max_miss_rate
+        self.min_miss_rate = min_miss_rate
+        self.expand_ratio = expand_ratio
+        self.shrink_ratio = shrink_ratio
 
     def set_mode(self, mode):
         self.mode = mode
